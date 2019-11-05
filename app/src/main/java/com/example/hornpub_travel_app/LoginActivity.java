7975ec375b79565308f62920be785ec850b931c2
NGUYỄN VĂN THẮNG <1712758@student.hcmus.edu.vn>
2019-11-05 21:07:27 +0700

    Sign up activity

--- app/src/main/java/com/example/hornpub_travel_app/LoginActivity.java
+++ app/src/main/java/com/example/hornpub_travel_app/LoginActivity.java
@@ -2,13 +2,32 @@ package com.example.hornpub_travel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+   TextView tvSignUp;
+   Intent intentToSignUp;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_login);
+      intentToSignUp = new Intent(this, SignUpActivity.class);
+      mapping();
+
+      tvSignUp.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            intentToSignUp.putExtra("isAdd", false);
+            startActivity(intentToSignUp);
+         }
+      });
+   }
+
+   private void mapping() {
+      tvSignUp = findViewById(R.id.textViewSignUp);
    }
 }
