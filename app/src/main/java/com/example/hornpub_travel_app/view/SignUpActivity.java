87f21a01517f6e529214cb53c010547b9ee85de1
NGUYỄN VĂN THẮNG <1712758@student.hcmus.edu.vn>
2019-11-08 09:45:15 +0700

    +check data, !signup
    Kiểm tra dữ liệu đăng kí, sử dụng scroll view cho màn hình đăng kí

--- app/src/main/java/com/example/hornpub_travel_app/view/SignUpActivity.java
+++ app/src/main/java/com/example/hornpub_travel_app/view/SignUpActivity.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.hornpub_travel_app.R;
+import com.example.hornpub_travel_app.model.LoginRequest;
+import com.example.hornpub_travel_app.model.RegisterRequest;
+import com.example.hornpub_travel_app.model.RegisterResponse;
+
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
    EditText editTextFullName, editTextEmail, editTextPhone, editTextPassWord, editTextConfirmPass;
    Button buttonSignUp;
-
+   RegisterRequest registerRequest;
+   RegisterResponse registerResponse;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +29,27 @@ public class SignUpActivity extends AppCompatActivity {
       buttonSignUp.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
-            checkData();
+            if (checkData()) sendRegisterRequest(registerRequest);
+
          }
 
       });
+   }
+
+   private void sendRegisterRequest(RegisterRequest registerRequest) {
+
    }
 
    private void mapping() {
       editTextFullName = findViewById(R.id.editTextFullName);
       editTextEmail = findViewById(R.id.editTextEmail);
       editTextPhone = findViewById(R.id.editTextPhone);
-      editTextPassWord = findViewById(R.id.editTextPass);
+      editTextPassWord = findViewById(R.id.editTextPassWord);
       editTextConfirmPass = findViewById(R.id.editTextConfirmPassWord);
       buttonSignUp = findViewById(R.id.buttonSignUp);
    }
 
-   private void checkData() {
+   private Boolean checkData() {
       String fullName, email, phone, password, confirmPass;
       fullName = editTextFullName.getText().toString();
       email = editTextEmail.getText().toString();
@@ -45,5 +57,38 @@ public class SignUpActivity extends AppCompatActivity {
       password = editTextPassWord.getText().toString();
       confirmPass = editTextConfirmPass.getText().toString();
 
+      if (!checkPassword(password, confirmPass)) return false;
+
+      registerRequest = new RegisterRequest(email, password, phone, fullName, null, null, null);
+      return true;
    }
+
+   private Boolean checkEmail(String email) {
+      if (email.length() < 1) {
+         editTextEmail.setError("Email không được rỗng");
+         return false;
+      }
+      String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+              "[a-zA-Z0-9_+&*-]+)*@" +
+              "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+              "A-Z]{2,7}$";
+      Pattern pattern = Pattern.compile(emailRegex);
+      if (pattern.matcher(email).matches()) {
+         editTextEmail.setError("Email không hợp lệ");
+         return false;
+      } else return true;
+   }
+
+   private Boolean checkPassword(String password, String confirmPass) {
+      if (password.length() < 5) {
+         editTextPassWord.setError("Mật khẩu quá ngắn, vui lòng nhập thêm");
+         return false;
+      }
+      if (!password.equals(confirmPass)) {
+         editTextConfirmPass.setError("Xác nhận mật khẩu không đúng");
+         return false;
+      } else return true;
+   }
+
+
 }
